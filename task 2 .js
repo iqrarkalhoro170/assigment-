6177c0d4aa6bee957c22dfbd5319dff35c3ffe67
iqrarkalhoro170 <hiqrar290@gmail.com>
2026-08-02 13:19:22 +0500

    Add files via upload

---
+++ task 2 .js
@@ -0,0 +1,31 @@
+let i = -5;
+
+while (i <= 5) {
+    if (i > 0) {
+        console.log(i + " = Positive");
+    } else if (i < 0) {
+        console.log(i + " = Negative");
+    } else {
+        console.log(i + " = Zero");
+    }
+
+    i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
